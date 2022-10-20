95c9d067571bb9956288b428bce729ef2c3a5cc1
eheh12321 <eheh12321@gmail.com>
2022-10-20 14:05:43 +0900

    Create Project

---
+++ src/main/java/lee/hyeong/smartnoticev2/SmartNoticeV2Application.java
@@ -0,0 +1,13 @@
+package lee.hyeong.smartnoticev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartNoticeV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartNoticeV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/lee/hyeong/smartnoticev2/SmartNoticeV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.hyeong.smartnoticev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartNoticeV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
